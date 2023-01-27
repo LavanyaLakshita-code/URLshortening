b4acbf07130659e37e91ac2b6712a56d401f99fe
lavanyal <lavanyalakshita287@gmail.com>
2023-01-27 16:14:17 +0530

    handled errors

--- index.js
+++ index.js
@@ -10,52 +10,65 @@
 //         })
 // }
 
-// Using Axios
-function postData() {
-    let url = document.getElementById("inputUrl").value;
-    // console.log(url);
-    axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`)
-        .then((response) => {
-            document.getElementById('contentDiv').style.display = "flex";
-            document.getElementById("getUrl").innerHTML = response.data.result.short_link;
-            document.getElementById('er-msg').style.display = "none";
-            document.getElementById('dot').style.display = "none";
-            document.getElementById("inputUrl").style.border = "none";
-        }).catch(function (error) {
-            if (error.response.data.error_code) {
-                let err = error.response.data.error;
-                if (err.length % 2 == 0 || err.length % 2 !== 0) {
-                    let extract = err.slice(0, err.length / 2);
-                    document.getElementById("er-msg").innerHTML = extract;
-                    document.getElementById('dot').style.display = "flex";
-                    document.getElementById('contentDiv').style.display = "none";
-                }
-                // document.getElementById("er-msg").innerHTML = error.response.data.error;
-                const element = document.getElementById("dot");
-                element.addEventListener("click", () => {
-                    element.innerHTML = error.response.data.error;
-                    document.getElementById('er-msg').style.display = "none";
-                });
-                document.getElementById("inputUrl").style.border = "1px solid red";
+// global
+let inpUrl = document.getElementById("inputUrl");
+let errMsg = document.getElementById('er-msg');
+let conDiv = document.getElementById('contentDiv');
+let getURL = document.getElementById("getUrl");
+let ellipsis = document.getElementById('dot');
 
-            } else {
-                alert("Unknown Error")
-            }
-        })
+// Api fetch Using Axios
+function postData() {
+    let url = inpUrl.value;
+    if (url.length == 0) {
+        errMsg.innerHTML = 'No URL specified ("url" parameter is empty)';
+        inpUrl.style.border = "1px solid red";
+        return false;
+    } else {
+        axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`)
+            .then((response) => {
+                conDiv.style.display = "flex";
+                getURL.innerHTML = response.data.result.short_link;
+                errMsg.style.display = "none";
+                ellipsis.style.display = "none";
+                inpUrl.style.border = "none";
+            }).catch(function (error) {
+                if (error.response.data.error_code) {
+                    let err = error.response.data.error;
+                    if (err.length % 2 == 0 || err.length % 2 !== 0) {
+                        let extract = err.slice(0, err.length / 2);
+                        errMsg.innerHTML = extract;
+                        errMsg.style.display = "flex";
+                        ellipsis.style.display = "flex";
+                        conDiv.style.display = "none";
+                    }
+                    ellipsis.addEventListener("click", () => {
+                        ellipsis.innerHTML = error.response.data.error;
+                        errMsg.style.display = "none";
+                    });
+                    inpUrl.style.border = "1px solid red";
+                } else {
+                    alert("Unknown Error")
+                }
+            })
+    }
 }
 
 const copyContent = async () => {
     let text = document.getElementById('getUrl').innerHTML;
-    // console.log(text);
+    let btn = document.getElementById("button");
     try {
         await navigator.clipboard.writeText(text);
-        setTimeout(function () {
-            var x = document.getElementById("button").innerHTML = "Copied";
-            const disable = document.querySelector('#button');
-            disable.style.backgroundColor = "grey";
-            disable.style.pointerEvents = "none";
-            disable.disabled = true;
-        });
+        btn.innerHTML = "Copied";
+        btn.style.backgroundColor = "grey";
+        btn.style.pointerEvents = "none";
+        btn.disabled = true;
+        setTimeout(() => {
+            btn.innerHTML = "Copy";
+            btn.style.backgroundColor = "cyan";
+            btn.style.pointerEvents = "auto";
+            btn.disabled = false;
+        }, 3000);
     } catch (err) {
         console.error('Failed to copy: ', err);
     }
